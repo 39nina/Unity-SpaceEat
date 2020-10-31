3572f613b261a485c8fdd320ecc57b5e1012b59f
39nina <392mknn@gmail.com>
2020-10-31 13:51:08 +0900

    Add：Start時の素材等配置

---
+++ Assets/FruitsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitsController : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody2D fruit = GetComponent<Rigidbody2D>();
+        float x = Random.Range(-3.0f, 3.0f);
+        Vector2 startForce = new Vector2(x, 4.0f);
+        fruit.AddForce(startForce);
+    }
+
+    void Update()
+    {
+        
+    }
+}
