696c6bebf76e74c4d719a93c442d1137b0abd821
39nina <392mknn@gmail.com>
2020-11-01 20:52:49 +0900

    Fix：直前のコミットと同一修正内容

---
+++ Assets/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    Rigidbody2D foodRig;
+
+    void Start()
+    {
+        foodRig = GetComponent<Rigidbody2D>();
+        float xPos = Random.Range(-700.0f, 700.0f);
+        Vector2 startForce = new Vector2(xPos, 0);
+        foodRig.AddForce(startForce);
+    }
+
+    void Update()
+    {
+        // 動きが一定以下になったら強制的に止める
+        if (foodRig.velocity.magnitude < 0.005)
+        {
+        foodRig.velocity = Vector2.zero;
+        foodRig.isKinematic = true;
+        }
+    }
+}
---
+++ Assets/FoodGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class FoodGenerator : MonoBehaviour
+{
+    public GameObject ApplePrefab, CherryPrefab, StrawberryPrefab, WaterMelonPrefab;
+
+    // スタート時にフルーツを生成
+    void StartOccurFood(GameObject foodPrefab)
+    {
+        GameObject startPrefab = Instantiate(foodPrefab) as GameObject;
+        startPrefab.transform.position = new Vector2(0, 4);
+    }
+
+    void Start()
+    {
+        StartOccurFood(ApplePrefab);
+        StartOccurFood(ApplePrefab);
+        StartOccurFood(CherryPrefab);
+        StartOccurFood(CherryPrefab);
+        StartOccurFood(StrawberryPrefab);
+        StartOccurFood(StrawberryPrefab);
+        StartOccurFood(WaterMelonPrefab);
+        StartOccurFood(WaterMelonPrefab);
+    }
+
+    void Update()
+    {
+
+    }
+}
